6f4869320d26b284fed2aacaba1d8122f4b0a8b8
zhangandi <zhangandi@360.cn>
2019-10-11 10:16:01 +0800

    add dotenv

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+mongoose.connect(process.env.DB_CONNECT,
+  { useUnifiedTopology: true,useNewUrlParser: true },
+  () => {
+    console.log('connect to db!')
+  }
+)
+
+// Import Routes
+const authRoute = require('./routes/auth')
+
+// Route Middleware
+// 相当于后面的请求都带此前缀，类似于baseURL
+app.use('/api/user', authRoute)
+
+app.listen(3000,() => console.log('Server Up and running!'))
---
+++ step.md
@@ -0,0 +1,15 @@
+# 初始化环境
+## 1.npm
+- nodemon
+注意：如果nodemon非全局安装需要用npm script去启动
+- express
+## 2.目录
+root
+  -node_modules
+  -routes
+    -auth.js
+  index.js
+
+## 3.安装mongoose
+npm install mongoose
+## 4.安装dotenv
